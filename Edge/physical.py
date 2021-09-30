ee745710503ea14d2b239924762adf30f9f48cef
Aditya Mitra <adityaarghya0@gmail.com>
2021-09-30 17:49:57 +0530

    Update physical.py

--- Edge/physical.py
+++ Edge/physical.py
@@ -1,9 +1,11 @@
 import serial
+import time
 
 def writeToArduino():
   for i in range(9):
     try:
       ser=serial.Serial('/dev/ttyUSB'+str(i),9600)
+      time.sleep(3)
       ser.write(b'1')
     except:
       pass
