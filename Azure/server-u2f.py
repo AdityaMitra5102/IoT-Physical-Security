9b5c12b405caee290d216c0217c500fc1a320275
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2021-10-01 15:00:04 +0530

    Update server-u2f.py

--- Azure/server-u2f.py
+++ Azure/server-u2f.py
@@ -1,29 +1,4 @@
-# Copyright (c) 2018 Yubico AB
-# All rights reserved.
-#
-#   Redistribution and use in source and binary forms, with or
-#   without modification, are permitted provided that the following
-#   conditions are met:
-#
-#    1. Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#    2. Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+
 
 """
 Example demo server to use a supported web browser to call the WebAuthn APIs
@@ -51,12 +26,10 @@ app = Flask(__name__, static_url_path="")
 app.secret_key = os.urandom(32)  # Used for session.
 
 rp = PublicKeyCredentialRpEntity("localhost", "Demo server")
-# By using the U2FFido2Server class, we can support existing credentials
-# registered by the legacy u2f.register API for an appId.
+
 server = U2FFido2Server("https://localhost:5000", rp)
 
-# Registered credentials are stored globally, in memory only. Single user
-# support, state is lost when the server terminates.
+
 credentials = []
 
 
@@ -130,13 +103,7 @@ def authenticate_complete():
     return cbor.encode({"status": "OK"})
 
 
-###############################################################################
-# WARNING!
-#
-# The below functions allow the registration of legacy U2F credentials.
-# This is provided FOR TESTING PURPOSES ONLY. New credentials should be
-# registered using the WebAuthn APIs.
-###############################################################################
+
 
 
 @app.route("/api/u2f/begin", methods=["POST"])
